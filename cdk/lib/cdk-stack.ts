6d109bc0425956ae577c6015d078518d9620ba3e
Binh Bui <bvbinh@sk-global.biz>
2025-02-04 11:29:53 +0700

    Add EC2 instance auto on/off scheduling and output public IP

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -8,6 +8,8 @@ import { CfnOutput } from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import { LinuxEc2InstanceProps, LinuxEc2Instance } from './linux-ec2';
+import { Rule, Schedule } from 'aws-cdk-lib/aws-events';
+import * as ec2_targets from 'aws-cdk-lib/aws-events-targets';
 
 export interface CdkStackProps extends cdk.StackProps {
   stage: string;
@@ -18,7 +20,7 @@ export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: CdkStackProps) {
     super(scope, id, props);
 
-    new LinuxEc2Instance(this, 'EC2Instance1', {
+    const ec2_instance = new LinuxEc2Instance(this, 'EC2Instance1', {
       instanceType: 't2.small',
       amiId: 'ami-06650ca7ed78ff6fa', // Ubuntu 24.04 LTS amd64
       keyName: props.keyName,
@@ -34,6 +36,41 @@ export class CdkStack extends cdk.Stack {
       subnetType: ec2.SubnetType.PUBLIC, // put EC2 in public subnet, so it can access the internet
       ebsVolumeSize: 40,
       osType: 'ubuntu',
+    });
+
+    // setup auto on/off for EC2 instance using CloudWatch Events
+    // on from 8am to 8pm, off from 8pm to 8am(GMT+7)
+    const onRule = new Rule(this, 'EC2OnRule', {
+      // start EC2 instance at 1am UTC = 8am GMT+7
+      schedule: Schedule.cron({ minute: '0', hour: '1', weekDay: 'MON-FRI' }),
+      description: 'Turn on EC2 instance',
+    });
+
+    const offRule = new Rule(this, 'EC2OffRule', {
+      // stop EC2 instance at 13pm UTC = 8pm GMT+7
+      schedule: Schedule.cron({ minute: '0', hour: '13', weekDay: 'MON-FRI' }),
+      description: 'Turn off EC2 instance',
+    });
+
+    onRule.addTarget(new ec2_targets.AwsApi({
+      service: 'EC2',
+      action: 'startInstances',
+      parameters: {
+        InstanceIds: [ec2_instance.instance.instanceId],
+      },
+    }));
+
+    offRule.addTarget(new ec2_targets.AwsApi({
+      service: 'EC2',
+      action: 'stopInstances',
+      parameters: {
+        InstanceIds: [ec2_instance.instance.instanceId],
+      },
+    }));
+
+    // Output the public IP of the EC2 instance
+    new CfnOutput(this, 'EC2PublicIp', {
+      value: ec2_instance.instance.instancePublicIp,
     });
   }
 }
