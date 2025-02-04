e515e1f53867bafee1b583f33201d4d6e349f307
Binh Bui <bvbinh@sk-global.biz>
2025-02-04 10:40:33 +0700

    Update EC2 instance details, remove unused resources, and upgrade Docker Compose version

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -18,63 +18,6 @@ export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: CdkStackProps) {
     super(scope, id, props);
 
-    // this repository is used to store the API image
-    const ecrRepository = new ecr.Repository(this, 'ecrRepository', {
-      repositoryName: `${id}-api`,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteImages: true,
-    });
-
-    // create a new S3 bucket, with versioning enabled
-    // this bucket is used to store the static data
-    const bucket = new s3.Bucket(this, 'Bucket', {
-      versioned: true,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-
-      publicReadAccess: false, // Block public access to the bucket and all of its contents
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
-
-      // CORS configuration
-      cors: [
-        {
-          allowedOrigins: ['*'],
-          allowedMethods: [
-            s3.HttpMethods.HEAD,
-            s3.HttpMethods.GET,
-            s3.HttpMethods.PUT,
-            s3.HttpMethods.POST,
-            s3.HttpMethods.DELETE,
-          ],
-          maxAge: 3000,
-          exposedHeaders: ['ETag'],
-          allowedHeaders: ['*'],
-        },
-      ],
-    });
-
-    // Thêm bucket policy để cho phép quyền truy cập công khai trong thư mục 'public'
-    bucket.addToResourcePolicy(
-      new iam.PolicyStatement({
-        effect: iam.Effect.ALLOW,
-        principals: [new iam.AnyPrincipal()],
-        actions: ['s3:GetObject'],
-        resources: [`${bucket.bucketArn}/public/*`], // Chỉ áp dụng cho thư mục public và các thư mục con
-      })
-    );
-
-    // policy to allow the EC2 instance to access the S3 bucket
-    const s3Policy = new iam.PolicyStatement({
-      actions: ['s3:*'],
-      resources: [bucket.bucketArn, `${bucket.bucketArn}/*`],
-    });
-
-    // policy for use Polly speech synthesis
-    const pollyPolicy = new iam.PolicyStatement({
-      actions: ['polly:SynthesizeSpeech'],
-      resources: ['*'],
-    });
-
     new LinuxEc2Instance(this, 'EC2Instance1', {
       instanceType: 't2.small',
       amiId: 'ami-06650ca7ed78ff6fa', // Ubuntu 24.04 LTS amd64
@@ -88,25 +31,9 @@ export class CdkStack extends cdk.Stack {
         { port: 80, description: 'Allow HTTP' },
         { port: 443, description: 'Allow HTTPS' },
       ],
-      subnetType: ec2.SubnetType.PUBLIC,
+      subnetType: ec2.SubnetType.PUBLIC, // put EC2 in public subnet, so it can access the internet
       ebsVolumeSize: 40,
       osType: 'ubuntu',
-      rolePolicyStatements: [s3Policy, pollyPolicy],
-    });
-
-    // output the ecr repository name
-    new CfnOutput(this, 'ecrRepositoryName', {
-      value: ecrRepository.repositoryName,
-    });
-
-    // output the ecr repository uri
-    new CfnOutput(this, 'ecrRepositoryUri', {
-      value: ecrRepository.repositoryUri,
-    });
-
-    // output the bucket name
-    new CfnOutput(this, 'bucketName', {
-      value: bucket.bucketName,
     });
   }
 }
